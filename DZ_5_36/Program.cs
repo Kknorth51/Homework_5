252ff03b744b0d2dc1baa8f21710e91688a107cf
kknorth <kirillacid@gmail.com>
2022-12-12 13:36:37 +0700

    Задача 36: Задайте одномерный массив, заполненный случайными числами.

---
+++ DZ_5_36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+CreateArrRandNumbs(numbers);
+Console.WriteLine("Сгенерированный массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int i = 1; i < numbers.Length; i+=2)
+    sum = sum + numbers[i];
+    Console.WriteLine($"Всего {numbers.Length}; сумма элементов, стоящих на нечётных позициях равна {sum}");
+
+void CreateArrRandNumbs(int[] numbers)
+{
+    for(int j = 0; j < numbers.Length; j++)
+        {
+            numbers[j] = new Random().Next(1,100);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    for(int x = 0; x < numbers.Length; x++)
+        {
+            Console.Write(numbers[x] + " ");
+        }
+    Console.WriteLine();
+}
---
+++ DZ_5_38/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 38: Задайте массив случайных вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+CreateArrRandNumbs(numbers);
+Console.WriteLine("Сгенерированный массив вещественных чисел: ");
+PrintArr(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (numbers[j] > max)
+    {
+        max = numbers[j];
+    }
+    if (numbers[j] < min)
+    {
+        min = numbers[j];
+    }
+}
+
+Console.WriteLine($"всего {numbers.Length}. Max = {max}, Min = {min}");
+Console.WriteLine($"Разница между Max и Min значением = {max - min}");
+
+void CreateArrRandNumbs(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random(  ).Next(100,1000)) / 10;
+        }
+}
+void PrintArr(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.WriteLine();
+}
